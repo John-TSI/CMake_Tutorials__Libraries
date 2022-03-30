0d56e60f2af65efaa454adc214778db2942cfcfa
John Roughley <jroughley@softwareinstitute.com>
2022-03-30 14:16:18 +0100

    initial commit

---
+++ build/CMakeFiles/Libraries.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Libraries.dir/src/main.cpp.obj"
+  "CMakeFiles/Libraries.dir/src/main.cpp.obj.d"
+  "Libraries.exe"
+  "Libraries.exe.manifest"
+  "Libraries.pdb"
+  "libLibraries.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Libraries.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include "hello.hpp"
+
+int main()
+{
+	// say hello
+	hello::say_hello();
+	return 0;
+}
